177c708872b1ba66d2da66bb9a5fefc5cbda10df
Hannes <Hannes.Holzwarth@me.com>
2016-02-16 21:23:32 +0100

    Fixed some typos etc.

--- examples/example1.js
+++ examples/example1.js
@@ -1,10 +1,10 @@
-var CSGameState = require('../index.js')();
+const CSGameState = new (require('../index.js'))(3000, '0.0.0.0');
 
-// The events ar being reffered to like objects
+// The events are being reffered to like objects
 // in this case the function will be called whenever 'player.state.health' changes
-CSGameState.on('player.state.health', function(health, oldValue, data) {
-    // If you need any other information (e.g. money) you can access it in the data object
+CSGameState.on('player.state.health', function(health, previous, data) {
+    // If you need any other information (e.g. money) you can access it via the data object
     // In the case of the players money that would be 'data.player.state.money'
-	console.log('Health of', data.player.name, 'has changed from', oldValue, 'to', health);
-    // OUTPUT: Health of Hansiiii has changed: 72
+	console.log('Health of', data.player.name, 'has changed from', previous, 'to', health);
+    // => OUTPUT: Health of Hansiiii has changed: 72
 });
